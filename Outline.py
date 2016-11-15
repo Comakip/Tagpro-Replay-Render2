ab26f0fa44566cd9e64ac7ad5ba90fa175da6367
DaEvil1 <thomas.m.bolstad@gmail.com>
2016-11-15 05:58:23 +0100

    fixed performance bug with labels

--- Outline.py
+++ Outline.py
@@ -17,12 +17,14 @@ class Outlined:
         self.text = pyglet.text.Label(string, font_name=font, font_size=size, x=x, 
                                       y=y, anchor_x = 'left', anchor_y = 'center', 
                                       batch=self.front)
+        self.text.color = self.data["color"]
         self.outline = []
         for i in self.offset:
             rx, ry = x + i[0], y + i[1]
             text = pyglet.text.Label(string, font_name=font, font_size=size, x=rx, 
                                     y=ry, anchor_x = 'left', anchor_y = 'center', 
                                     batch = self.back)
+            text.color = self.data["outline"]
             self.outline.append(text)
 
 
@@ -30,19 +32,19 @@ class Outlined:
         st, f, s = self.data["string"], self.data["font"], self.data["size"]
         x, y = self.data["coords"]
         color, outline = self.data["color"], self.data["outline"]
-        self.text.text = st
-        self.text.font = f
-        self.text.size = s
         self.text.x, self.text.y = x, y
-        self.text.color = color
+        self.text.text = st
+        #self.text.font = f
+        #self.text.size = s
+        #self.text.color = color
         for i in range(len(self.outline)):
             self.outline[i].text = st
-            self.outline[i].font = f
-            self.outline[i].size = s
+            #self.outline[i].font = f
+            #self.outline[i].size = s
             #print(x, self.offset[i])
             self.outline[i].x = x + self.offset[i][0]
             self.outline[i].y = y + self.offset[i][1]
-            self.outline[i].color = outline
+            #self.outline[i].color = outline
 
     def Draw(self):
         #for i in self.outline:
@@ -50,3 +52,4 @@ class Outlined:
         #self.text.draw()
         self.back.draw()
         self.front.draw()
+
