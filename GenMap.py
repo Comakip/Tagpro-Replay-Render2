639c94a829a86b5e2f063b9e952815b9e90ebbb6
DaEvil1 <da.evil.gaston@gmail.com>
2016-11-30 04:24:11 +0100

    updated to conform more with PEP8

--- GenMap.py
+++ GenMap.py
@@ -1,8 +1,12 @@
-from code_and_map import *
-from DecodeReplay import *
+import json
+import os
+import pyglet
+import sys
+
 from PIL import Image
 
-import sys, os, traceback, string, time, json, pyglet
+from DecodeReplay import *
+from code_and_map import *
 
 
 class GenMap:
@@ -13,16 +17,16 @@ class GenMap:
     files["boost"] = script_path + "/img/boost.png"
     files["blue boost"] = script_path + "/img/blue boost.png"
     files["red boost"] = script_path + "/img/bed boost.png"
-    functions = {"tiles" : tiles_map
-               #, "portal" : portal_map, "boost" : boost_map, 
-               #  "red boost" : boost_red_map, "blue boost" : boost_blue_map}
-               }
-    dynamic = ["bomb", "neutral flag", "red flag", "blue flag", 
-               "gate neutral", "gate red", "gate blue", "tagpro", "jukejuice", 
-               "rolling bomb", "mars ball", "portal", "portal off", 
-                "boost", "boost off", "red boost", "red boost off", "blue boost",
-                "blue boost off"]
-    with open('rotateCoords.json') as data_file:    
+    functions = {"tiles": tiles_map
+                 # , "portal" : portal_map, "boost" : boost_map,
+                 #  "red boost" : boost_red_map, "blue boost" : boost_blue_map}
+                 }
+    dynamic = ["bomb", "neutral flag", "red flag", "blue flag",
+               "gate neutral", "gate red", "gate blue", "tagpro", "jukejuice",
+               "rolling bomb", "mars ball", "portal", "portal off",
+               "boost", "boost off", "red boost", "red boost off", "blue boost",
+               "blue boost off"]
+    with open('rotateCoords.json') as data_file:
         smooth_coords = json.load(data_file)
 
     def _toStrings(self):
@@ -35,17 +39,15 @@ class GenMap:
                 pixels_h.append(cur_string)
             self.map_layout.append(pixels_h)
 
-
-
     def _back_tiles(self):
-        excluded = ("black", "wall", "floor", "gate neutral", "gate red", 
+        excluded = ("black", "wall", "floor", "gate neutral", "gate red",
                     "gate blue", "red endzone", "blue endzone")
         background = ("floor", "red speed", "blue speed",
                       "red endzone", "blue endzone", "black")
         self.back_tiles = []
         m_h = self.data["map height"]
         for i in range(len(self.tiles_id)):
-            #print(self.tiles_id[i - m_w], self.tiles_id[i], 
+            # print(self.tiles_id[i - m_w], self.tiles_id[i],
             #      self.tiles_id[i + m_w])
             if self.tiles_id[i] in excluded:
                 self.back_tiles.append(None)
@@ -66,13 +68,12 @@ class GenMap:
                         break
                 self.back_tiles.append(cur)
 
-
     def _gen_rects(self):
-        self.rects = [] 
+        self.rects = []
         self.tiles_id = []
         for i in range(len(self.map_layout)):
             for j in range(len(self.map_layout[i])):
-                new_rect = (i*40, j*40, (j+1)*40, (i+1)*40)
+                new_rect = (i * 40, j * 40, (j + 1) * 40, (i + 1) * 40)
                 self.rects.append(new_rect)
                 self.tiles_id.append(self.map_layout[i][j])
 
@@ -82,12 +83,10 @@ class GenMap:
         smooth_obj = Image.open(self.files["tiles"])
         for i in s_c:
             rx, ry = s_c[i]["x"], s_c[i]["y"]
-            rx, ry = int(rx*40), int(ry*40)
+            rx, ry = int(rx * 40), int(ry * 40)
             box = (rx, ry, rx + 20, ry + 20)
             newtile_sub = smooth_obj.crop(box)
             self.smooth[i] = newtile_sub
-
-
 
     def _gen_Tile_Objs(self):
         self.tiles = {}
@@ -112,49 +111,42 @@ class GenMap:
 
     def __init__(self, replay_data, win):
         self.t = 0
-        h =len(replay_data["map"][0])
+        h = len(replay_data["map"][0])
         w = len(replay_data["map"])
-        self.data = {}
-        self.data["height"] = h*40
-        self.data["width"] = w*40
-        self.data["map height"] = h
-        self.data["map width"] = w
-        self.data["win"] = win
+        self.data = {"height": h * 40, "width": w * 40, "map height": h, "map width": w, "win": win}
         self.last = False
         self.map_data = replay_data
         self._toStrings()
-        #self.tiles = pyglet.image.load(self.files["tiles"])
+        # self.tiles = pyglet.image.load(self.files["tiles"])
         self.s_batch = pyglet.graphics.Batch()
         self._gen_rects()
         self._back_tiles()
         self._gen_smooth()
         self._gen_Tile_Objs()
-        self.img = Image.new("RGBA", (w*40, h*40), "black")
+        self.img = Image.new("RGBA", (w * 40, h * 40), "black")
         self.follow = False
         self.data["player"] = self.focus()
         self.data["frame"] = 0
 
     def RenderMap(self):
-        m_height = self.data["height"]
         excluded = ["315 tile", "45 tile", "225 tile", "135 tile"]
-        off = {"gate red" : "gate off", "gate blue" : "gate off", 
-                "gate green" : "gate off", "gate off" : "gate off", 
-                "red flag" : "red flag away", "red flag away" : "red flag away", 
-                "blue flag" : "blue flag away", "blue flag away" : "blue flag away", 
-                "yellow flag" : "yellow flag away", 
-                "yellow flag away" : "yellow flag away", "bomb" : "bomb off", 
-                "bomb off" : "bomb off", "jukejuice" : "powerup off", 
-                "rolling bomb" : "powerup off", "tagpro" : "powerup off", 
-                "powerup off" : "powerup off"
-                }
-        newtile = pyglet.image.load(self.files["tiles"])
+        off = {"gate red": "gate off", "gate blue": "gate off",
+               "gate green": "gate off", "gate off": "gate off",
+               "red flag": "red flag away", "red flag away": "red flag away",
+               "blue flag": "blue flag away", "blue flag away": "blue flag away",
+               "yellow flag": "yellow flag away",
+               "yellow flag away": "yellow flag away", "bomb": "bomb off",
+               "bomb off": "bomb off", "jukejuice": "powerup off",
+               "rolling bomb": "powerup off", "tagpro": "powerup off",
+               "powerup off": "powerup off"
+               }
         for i in range(len(self.back_tiles)):
             if self.back_tiles[i]:
                 rect = (self.rects[i][0], self.rects[i][1])
                 subtile = self.tiles[self.back_tiles[i]]
                 self.img.paste(subtile, rect, mask=subtile)
         for i in range(len(self.tiles_id)):
-           if self.tiles_id[i] not in self.dynamic and self.tiles_id[i] not in excluded:
+            if self.tiles_id[i] not in self.dynamic and self.tiles_id[i] not in excluded:
                 rect = (self.rects[i][0], self.rects[i][1])
                 subtile = self.tiles[self.tiles_id[i]]
                 self.img.paste(subtile, rect, mask=subtile)
@@ -164,7 +156,7 @@ class GenMap:
                 subtile = self.tiles[off[self.tiles_id[i]]]
                 self.img.paste(subtile, rect, mask=subtile)
         w_map = self.map_data["wallMap"]
-        rel_coord = [(0, 0), (0, 20), (20,20), (20, 0)]
+        rel_coord = [(0, 0), (0, 20), (20, 20), (20, 0)]
         s_c = self.smooth_coords
         for i in range(len(w_map)):
             for j in range(len(w_map[i])):
@@ -172,31 +164,29 @@ class GenMap:
                     if w_map[i][j][k] != 0:
                         key = w_map[i][j][k]
                         rx, ry = s_c[key]["x"], s_c[key]["y"]
-                        square = rx*40, ry*40, 20, 20
-                        y = j*40 + rel_coord[k][0]
-                        y2 = y - 20
-                        x = i*40 + rel_coord[k][1]
-                        x2 = x + 20
+                        y = j * 40 + rel_coord[k][0]
+                        x = i * 40 + rel_coord[k][1]
                         self.img.paste(self.smooth[key], (x, y), mask=self.smooth[key])
-        self.img.save("test.png")
-        self.img = self.img.transpose(Image.FLIP_TOP_BOTTOM)
-        raw_img = self.img.tobytes()
+        raw_img = self.img.transpose(Image.FLIP_TOP_BOTTOM)
+        raw_img = raw_img.tobytes()
         w, h = self.data["width"], self.data["height"]
         raw_img = pyglet.image.ImageData(w, h, "RGBA", raw_img)
         spr_ = pyglet.sprite.Sprite(raw_img)
         spr_.x, spr_.y = 0, 0
         self.sprite = spr_
+        self.img = self.img.convert("RGB")
+        self.img.save("test.png")
 
     def draw(self, dt):
         frame = self.data["frame"]
         w, h = self.data["win"]
         mh = self.data["height"]
         if self.follow:
-            self.sprite.x = int(w/2 - self.data["player"]["x"][frame] - 20)
-            self.sprite.y = int(h/2 -mh + self.data["player"]["y"][frame] + 20)
+            self.sprite.x = int(w / 2 - self.data["player"]["x"][frame] - 20)
+            self.sprite.y = int(h / 2 - mh + self.data["player"]["y"][frame] + 20)
         self.sprite.draw()
         self.data["frame"] += 1
-        self.data["frame"] = self.data["frame"] % len(self.data["player"]["x"])
+        self.data["frame"] %= len(self.data["player"]["x"])
 
     def __str__(self):
         out = ""
@@ -205,10 +195,12 @@ class GenMap:
                 out += j + ", "
         return out
 
+
 def back(dt):
     quad = (0, 1300, 1300, 1300, 1300, 0, 0, 0)
     color = 'c3B', (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
     pyglet.graphics.draw(4, pyglet.gl.GL_QUADS, ('v2f', quad), color)
+
 
 def main():
     w, h = 1080, 800
@@ -221,14 +213,11 @@ def main():
     win = pyglet.window.Window(w, h, visible=False, caption="", vsync=0)
     win.set_visible()
     win.PYGLET_VSYNC = 0
-    pyglet.clock.schedule(back)  
-    pyglet.clock.schedule(tp_map.draw)  
-    #tp_map.RenderMap(win)
+    pyglet.clock.schedule(back)
+    pyglet.clock.schedule(tp_map.draw)
+    # tp_map.RenderMap(win)
     pyglet.app.run()
-    
-
 
 
 if __name__ == "__main__":
     main()
-
--- Players.py
+++ Players.py
@@ -1,21 +1,19 @@
-from code_and_map import *
-from DecodeReplay import *
-from GenMap import *
 from DynamicElements import *
 from Outline import *
 from PIL import Image
 from pyglet.window import key
 
 import pyglet
+import sys
+import os
 
-import sys, os, traceback, string, time, json, random, datetime
 
 class Players:
     files = {}
     script_path = os.getcwd()
     files["tiles"] = script_path + "/img/tiles.png"
-    files["flairs"] =script_path + "/img/flair.png"
-    functions = {"tiles" : tiles_map
+    files["flairs"] = script_path + "/img/flair.png"
+    functions = {"tiles": tiles_map
                  }
     balls = ("blue ball", "red ball")
     flag = ("neutral flag", "red flag", "blue flag")
@@ -31,20 +29,20 @@ class Players:
                 n_img = subtile.rotate(j, resample=Image.BICUBIC)
                 n_img = n_img.tobytes()
                 pyg_img = pyglet.image.ImageData(40, 40, "RGBA", n_img)
-                pyg_img.anchor_x = int(pyg_img.width/2)
-                pyg_img.anchor_y = int(pyg_img.height/2)
+                pyg_img.anchor_x = int(pyg_img.width / 2)
+                pyg_img.anchor_y = int(pyg_img.height / 2)
                 pyg_spr = pyglet.sprite.Sprite(pyg_img)
                 self.tiles[i].append(pyg_spr)
         for i in self.flag:
             newtile = pyglet.image.load(self.files["tiles"])
             h = newtile.height
             rx, ry = self.functions["tiles"][i]
-            subtile = newtile.get_region(x=rx, y=h-40 -ry, width=40, height=40)
+            subtile = newtile.get_region(x=rx, y=h - 40 - ry, width=40, height=40)
             subtile = pyglet.sprite.Sprite(subtile, batch=self.batch)
             self.tiles[i] = subtile
 
     def _gen_flairs(self):
-        #flairs
+        # flairs
         flairs = pyglet.image.load((self.files["flairs"]))
         grid = pyglet.image.ImageGrid(flairs, 10, 11)
         self.flairs = pyglet.image.TextureGrid(grid)
@@ -56,7 +54,6 @@ class Players:
             text = Outlined(i, "Times New Roman", 10, 0, 0)
             self.names[i] = text
 
-
     def focus(self):
         for i in self.players:
             if self.players[i]["me"] == "me":
@@ -64,12 +61,8 @@ class Players:
 
     def __init__(self, replay_data, win):
         self.t = 0
-        self.data = {}
-        self.data["frame"] = 0
-        self.data["replay"] = replay_data
-        self.data["width"] = len(replay_data["map"])
-        self.data["height"] = len(replay_data["map"][0])
-        self.data["win"] = win
+        self.data = {"frame": 0, "replay": replay_data, "width": len(replay_data["map"]),
+                     "height": len(replay_data["map"][0]), "win": win}
         n = 0
         self.players = {}
         self.batch = pyglet.graphics.Batch()
@@ -89,13 +82,13 @@ class Players:
         frame = self.data["frame"]
         angle = cur["angle"][frame]
         if angle:
-            angle = int(angle*360/(2*3.14))
+            angle = int(angle * 360 / (2 * 3.14))
         return angle
 
     def _drawplayer(self, p_data):
         x, y, draw, dead, team, name, angle = p_data[0:7]
         if draw and not dead and team:
-            #if angle:
+            # if angle:
             #    if angle != self.p_angle:
             #        self.tiles[team].rotation = angle
             #        self.tiles[team].scale = 1
@@ -105,7 +98,7 @@ class Players:
 
     def _drawname(self, p_data, player):
         x, y, draw, dead, team, name = p_data[0:6]
-        x,y = int(x), int(y)
+        x, y = int(x), int(y)
         if draw and not dead and team:
             self.names[player].data["coords"] = (x + 18, y + 32)
             self.names[player].data["string"] = name
@@ -118,64 +111,62 @@ class Players:
             self.tiles[flag].x, self.tiles[flag].y = x - 7, y + 11
             self.tiles[flag].draw()
 
-
     def _drawflair(self, p_data):
         x, y, draw, dead, team, name, angle, flag, flair = p_data[0:9]
-        h = self.flairs.height/16
+        h = self.flairs.height / 16
         if flair and draw and not dead and team:
             rx, ry = flair["x"], h - 1 - flair["y"]
             rx, ry = int(rx), int(ry)
-            pos = ry*11 + rx
+            pos = ry * 11 + rx
             flair_t = self.flairs[(pos)]
             subtile = pyglet.sprite.Sprite(flair_t)
-            subtile.x, subtile.y = x  - 6, y + 27
+            subtile.x, subtile.y = x - 6, y + 27
             subtile.draw()
 
-
-    def NewFrame(self, dt, offset = None):
+    def NewFrame(self, dt, offset=None):
         last = len(self.data["replay"]["score"])
-        bool = {"false" : False, "true" : True}
-        teams = {2 : "blue ball", 1 : "red ball", 0 : False, None : False}
-        flags = {1 : "red flag", 2 : "blue flag", 3 : "neutral flag", 0 : False, 
-                 None : False}
+        bool = {"false": False, "true": True}
+        teams = {2: "blue ball", 1: "red ball", 0: False, None: False}
+        flags = {1: "red flag", 2: "blue flag", 3: "neutral flag", 0: False,
+                 None: False}
         frame = self.data["frame"]
         players = self.players
         h = self.data["height"]
         w = self.data["width"]
         p_data = {}
-        #timea = time.time()
+        # timea = time.time()
         for i in players:
             cur = players[i]
             x, y = cur["x"][frame], cur["y"][frame]
             if offset and x and y:
-                x, y = x + 20, h*40 - y - 20
+                x, y = x + 20, h * 40 - y - 20
                 x, y = x + offset[0], y + offset[1]
                 x, y = int(x), int(y)
             flag = flags[cur["flag"][frame]]
             angle = self._angle(cur)
             team = teams[cur["team"][frame]]
-            draw, dead = cur["draw"][frame],  cur["dead"][frame]
+            draw, dead = cur["draw"][frame], cur["dead"][frame]
             name, flair = cur["name"][frame], cur["flair"][frame]
             p_data[i] = [x, y, draw, dead, team, name, angle, flag, flair]
             self._drawplayer(p_data[i])
             if flag:
                 self._drawflag(p_data[i])
-        #for i in p_data:
-            #self._drawflair(p_data[i])
-            #self._drawname(p_data[i], i)
-        #timeb = time.time()
-        #print(timeb - timea)
+                # for i in p_data:
+                # self._drawflair(p_data[i])
+                # self._drawname(p_data[i], i)
+        # timeb = time.time()
+        # print(timeb - timea)
         self.data["frame"] += 1
-        self.data["frame"] = self.data["frame"] % last
+        self.data["frame"] %= last
 
     def on_key_press(self, button, modifiers):
         frame = self.data["frame"]
         if self.keys[key.LEFT]:
-            frame -= 60*5
+            frame -= 60 * 5
         if self.keys[key.RIGHT]:
-            frame += 60*5
+            frame += 60 * 5
         if frame < 0:
-            frame = len(self.data["replay"]["score"]) - 60*5
+            frame = len(self.data["replay"]["score"]) - 60 * 5
             if frame < 0:
                 frame = 0
         if frame >= len(self.data["replay"]["score"]):
@@ -184,33 +175,31 @@ class Players:
         self.gmap.data["frame"] = frame
         self.data["frame"] = frame
 
-
-            
     def add_previous(self, gmap, dynamic):
         self.gmap = gmap
         self.dynamic = dynamic
 
     def draw(self, dt):
         w, h = self.data["win"][1:]
-        mh = self.data["height"]*40
+        mh = self.data["height"] * 40
         frame = self.data["frame"]
-        x = w/2 - self.data["player"]["x"][frame] - 20
-        y = h/2 - mh + self.data["player"]["y"][frame] + 20
+        x = w / 2 - self.data["player"]["x"][frame] - 20
+        y = h / 2 - mh + self.data["player"]["y"][frame] + 20
         offset = x, y
         self.NewFrame(dt, offset)
 
 
 def draw(dt, funcs, fps, win):
-    #timea = time.time()
-    #back()
+    # timea = time.time()
+    # back()
     win.clear()
     for i in funcs:
-        #i.data["frame"] += 3
+        # i.data["frame"] += 3
         i.draw(dt)
     fps.draw()
-    #timeb = time.time()
-    #print(timea - timeb)
-    #print(dt)
+    # timeb = time.time()
+    # print(timea - timeb)
+    # print(dt)
 
 
 def main():
@@ -228,14 +217,13 @@ def main():
     players.add_previous(Newmap, dynamic)
     win.on_key_press = players.on_key_press
     win.set_visible()
-    fps= pyglet.clock.ClockDisplay()
-    d_time = 1.0/60
+    fps = pyglet.clock.ClockDisplay()
+    d_time = 1.0 / 60
     funcs = (Newmap, dynamic, players)
-    #funcs = (dynamic, players)
+    # funcs = (dynamic, players)
     pyglet.clock.schedule_interval(draw, d_time, funcs, fps, win)
     pyglet.app.run()
 
 
 if __name__ == "__main__":
     main()
-
